5df5bc898e79108b39229b7b60b0d68c6d14c480
BuassaM <miguexcheide22@hotmail.comgit config --global user.name BuassaM>
2023-02-16 08:45:49 -0800

    task4uptd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
- #include <stdio.h>
-
- /**
-  * main - main Entry
-   * Return: 0
-   */
-
-  int main(void)
-  {
-	  puts("\"Programming is like building a multilingual puzzle");
-	  return (0);
-  }		  
+#include <stdio.h>
+/**
+ * main - Prints a line with a function
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}		  
