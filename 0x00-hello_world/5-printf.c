ed308c0d3b3aae68e33d62acbfcaad0b7ca0b154
BuassaM <miguexcheide22@hotmail.comgit config --global user.name BuassaM>
2023-02-16 08:07:11 -0800

    taskCupdt

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
-  #include <stdio.h>
- 
+  #include <stdio.h> 
   /**
    * main - main entry
    * Return: 0 (sucess)
    */
-
-  int main(void)
+ int main(void)
    {
-	   printf("with proper grammar, but the outcome is a piece of art,\n");
-	   return (0);
+     printf("with proper grammar, but the outcome is a piece of art,\n");
+     return (0);
    }
