4d2ed055ab1ebe712db91d6003d1d6083d089c54
BuassaM <miguexcheide22@hotmail.comgit config --global user.name BuassaM>
2023-02-17 05:40:18 -0800

    task01

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
-* main - function that assign a random number to the variable n each time it is executed.
+* main - function that assign a random number to the variable n
 *
 * Return: Always 0 (Success)
 */
@@ -14,10 +14,12 @@ n = rand() - RAND_MAX / 2;
 if (n < 0)
 {
 printf("%d is negative\n", n);
-} else if (n > 0)
+} 
+else if (n > 0)
 {
 printf("%d is positive\n", n);
-} else
+} 
+else
 {
 printf("%d is zero\n", n);
 }
