a2a5f3bcb1d1e57328ed0473ddb511e58afc9804
BuassaM <miguexcheide22@hotmail.comgit config --global user.name BuassaM>
2023-02-16 19:20:14 -0800

    task101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - function that prints text with standard error
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char stderr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+printf("%s\n", stderr);
+return (1);
+}
