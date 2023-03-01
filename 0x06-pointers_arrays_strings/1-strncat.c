16d8ec23955b72b6a54fe25130afa091f841153f
BuassaM <miguexcheide22@hotmail.comgit config --global user.name BuassaM>
2023-03-01 09:36:08 -0800

    Pointers-Arrays-Strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,31 +1,27 @@
 #include "main.h"
-
 /**
-* _strncat - concatenate two strings
-*
-* @n: number of characters in source stng
-* @dest: destination stng
-* @src: source 
-*
-* Return: new concat string
-*/
+ * _strncat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes used from src.
+ * Return: the pointer to dest.
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-int index_1, index_2;
+	int count = 0, count2 = 0;
 
-/* find length of destination stng */
-for (index_1 = 0; dest[index_1] != '\0'; index_1++)
-{
-continue;
-}
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
 
-/* find length of source stng and check if less than length of integer n */
-for (index_2 = 0; src[index_2] != '\0' && index_2 < n; index_2++)
-{
-dest[index_1 + index_2] = src[index_2]; /* concat strings */
-}
-
-dest[index_1 + index_2] = '\0'; 
-/* add null character to concat strings */
-return (dest);
+	while (count2 < n)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
+	return (dest);
 }
