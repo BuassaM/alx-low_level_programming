849fea26dd0161982b0c58f9ec20272c698daecf
BuassaM <miguexcheide22@hotmail.comgit config --global user.name BuassaM>
2023-03-20 04:03:15 -0700

    PreprocessorTasks

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 sucess
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,3 @@
+0x0D. C - Preprocessor
+
+0-object_like_macro.h: Create a header file that defines a macro named SIZE as an abbreviation for the token 1024.
